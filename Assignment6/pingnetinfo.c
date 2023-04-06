82227b9ac664027fa5c6442c87091f19df763b88
sidvisw <sid1232100@gmail.com>
2023-04-06 19:08:47 +0530

    gethostbyname done

--- Assignment6/pingnetinfo.c
+++ Assignment6/pingnetinfo.c
@@ -1,9 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
 
 int main(int argc, char *argv[])
 {
-    /* Code here */
+    if (argc != 4)
+    {
+        printf("Usage: %s <IP Address / Site Address> <Number of probes> <Time difference between probes>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    struct hostent *host = gethostbyname(argv[1]);
+    if (host == NULL)
+    {
+        printf("Unable to resolve the host name.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct in_addr dest_ip = *(struct in_addr *)host->h_addr_list[0];
+
+    
 
     return 0;
 }
