a3198b3781e84d9717a995171586a0760c480f0f
sidvisw <sid1232100@gmail.com>
2023-04-16 20:37:14 +0530

    final commit

--- Assignment6/mytraceroute.c
+++ Assignment6/mytraceroute.c
@@ -1,9 +1,3 @@
-/*
-    Execution Instructions:
-    gcc mytraceroute_19CS30008.c -o mytraceroute 
-    sudo ./mytraceroute www.example.com
-*/
-
 #include <arpa/inet.h>
 #include <assert.h>
 #include <dirent.h>
